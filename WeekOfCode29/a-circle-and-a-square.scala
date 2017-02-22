49393c21fe85f6f570725ab5107a6512808cbac0
jmromeroes <jmromeroe@gmail.com>
2017-02-22 17:04:38 -0500

    Added a-circle-and-a-square initial aproximation. Still a lot left for that problem

---
+++ WeekOfCode29/a-circle-and-a-square.scala
@@ -0,0 +1,5 @@
+val sizeList: List[Int] = scala.io.StdIn.readLine.split(" ").map(_.toInt)
+val size: (Int, Int) = (sizeList(0), sizeList(1))
+
+val circleList: List[Int] = scala.io.StdIn.readLine.split(" ").map(_.toInt) 
+val circlePosition: (Int, Int) = circleList()
