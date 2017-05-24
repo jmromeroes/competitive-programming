d74b9cfd8415bc4b73b25db6d21259c79d67c9ff
jmromeroes <jmromeroe@gmail.com>
2017-05-23 23:16:07 -0500

    Adding final AVL tree operations implementations

--- DataStructuresTrees/self-balancing-tree.java
+++ DataStructuresTrees/self-balancing-tree.java
@@ -50,33 +50,63 @@ static int setHeight(Node root){
   return root.ht;
 }
 
+static Node rightRotate(Node root){
+    Node aux = null;
+    Node r = root.left;
+        
+    if(root.left.right != null)
+        aux = root.left.right;
+    root.left.right = root;
+                  
+    root.left = aux;
+
+    return r;
+}
+
+static Node leftRotate(Node root){
+    Node aux = null;
+    Node r = root.right;
+        
+    if(root.right.left != null)
+        aux = root.right.left;          
+        
+    root.right.left = root;
+    root.right = aux;
+            
+    return r;
+}
+
+static Node leftRightRotate(Node root){
+    root.right = rightRotate(root.right);
+    return leftRotate(root);  
+}
+
+static Node rightLeftRotate(Node root){
+    root.left = leftRotate(root.left);
+    return rightRotate(root);  
+}
+
+static int getBalance(Node root){
+    return height(root.right) - height(root.left);  
+}
+
 static Node balance(Node root){
   if(root != null){
     root.right = balance(root.right);
     root.left = balance(root.left);
     
     if(height(root.left) - height(root.right) > 1){
-        Node aux = null;
-        Node r = root.left;
-        
-        if(root.left.right != null)
-            aux = root.left.right;
-        root.left.right = root;
-                  
-        root.left = aux;
-
-        return r;
+        if(getBalance(root.left) >= 0){
+          return rightLeftRotate(root);
+        }
+      
+        return rightRotate(root);
     } else if(height(root.right) - height(root.left) > 1){
-        Node aux = null;
-        Node r = root.right;
-        
-        if(root.right.left != null)
-            aux = root.right.left;          
-        
-        root.right.left = root;
-        root.right = aux;
-            
-        return r;
+        if(getBalance(root.right) <= 0){
+          return leftRightRotate(root);        
+        }
+      
+        return leftRotate(root);
     } else {        
         return root;
     }
@@ -89,14 +119,14 @@ static String tree(Node root){
   if(root == null){
     return " null ";
   } else {
-    return "Node " + root.val + " - " + root.ht + "(" + tree(root.left) + ", " + tree(root.right) + " )"; 
+    return "Node " + root.val + " - " + getBalance(root) + "(" + tree(root.left) + ", " + tree(root.right) + " )"; 
   } 
 }
 
 static Node insert(Node root,int val) {
     insert2(root, val);
-    Node res = balance(root);
-    
+    Node res = balance(root);    
     setHeight(res);
+  
     return res;
 }
