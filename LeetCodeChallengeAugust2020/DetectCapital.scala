81d72b3904f38e090ffc92404a7c4bf44feb55fa
José Miguel Romero Espitia <jmromeroe@gmail.com>
2020-08-01 17:48:21 -0500

    Adding simple problem

---
+++ LeetCodeChallengeAugust2020/DetectCapital.scala
@@ -0,0 +1,29 @@
+object Solution {
+    def detectCapitalUse(word: String): Boolean = {
+       if(word.length == 1) return true
+       
+       var firstCharUpper = word(0).isUpper
+       var secondCharUpper = word(1).isUpper
+        
+       var charCase: Int = (firstCharUpper, secondCharUpper) match {
+           case (true, true) => 1
+           case (true, false) => 2
+           case (false, false) => 3
+           case _ => 4
+       }
+        
+       if(charCase == 4) return false
+        
+       for(i <- 2 until word.length){
+          charCase match {
+             case 1 =>
+                if(!word(i).isUpper) return false
+             case 2 =>
+                if(word(i).isUpper) return false
+             case 3 =>
+                if(word(i).isUpper) return false
+          }    
+       }
+       true
+    }
+}
